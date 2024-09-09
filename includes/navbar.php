2879e3a1cb11dfe5cdb22ba13b45f2cf13fc89ee
edward0100 <166081339+edward0100@users.noreply.github.com>
2024-09-09 11:22:26 +0000

    folder containing navigation bar for the site

---
+++ includes/navbar.php
@@ -0,0 +1,38 @@
+<a?php
+session_start();
+?>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="index.php">E-Commerce Site</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="index.php">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="product_listing.php">Products</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="view_cart.php">Cart</a>
+            </li>
+            <?php if (isset($_SESSION['user_id'])): ?>
+                <li class="nav-item">
+                    <a class="nav-link" href="logout.php">Logout</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="profile.php">Profile</a>
+                </li>    
+            <?php else: ?>
+                <li class="nav-item">
+                    <a class="nav-link" href="login.php">Login</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="register.php">Register</a>
+                </li>
+            <?php endif; ?>
+        </ul>
+    </div>
+</nav>
